03021dfd9a6328ea59e4ab8df5a04dbf75e6034d
HYEJIN3 <aslk1111zz@naver.com>
2022-10-23 23:28:51 +0900

    init

---
+++ Desktop/portfolio/src/resumeData.js
@@ -0,0 +1,91 @@
+let resumeData = {
+    "imagebaseurl":"https://HYEBA.github.io/",
+    "name": "장혜진",
+    "role": "프론트엔드 개발자",
+    "roleDescription": "새로운 것을 배우고, 쉬지 않고 발전하기 위해 개발자의 꿈을 갖게 되었습니다",
+    "roleDescription2": "더 많은 것을 배우고, 코딩하고 싶습니다",
+    "socialLinks":[
+        {
+          "name":"github",
+          "url":"http://github.com/HYEJIN3",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"프론트엔드 2년차 개발자 장혜진입니다",
+    "aboutDescription":"html, css, javascript, node.js, mysql, nginX와 aws를 다루는 데 익숙하고, 기본적인 리눅스 명령어를 잘 숙지하고 있으며 React, three.js, canvas를 다룰 수 있습니다. 기본 웹 제작부터 배포까지 여러 번의 경험이 있고, 주요 성과는 블록체인 지갑 앱 랜딩페이지(단독 개발), 회사 어드민 페이지(단독 개발), 블록체인 지갑 앱, 회사 홈페이지 개발입니다",
+    "email":"aslk3366zz@naver.com",
+    "phone":"010-6332-2458",
+    "website":"https://HYEBA.github.io",
+    "education":[
+      {
+        "UniversityName":"한국산업기술대학교",
+        "specialization":"기계설계공학과",
+        "YearOfPassing":"2012.03 ~ 2020.02",
+        "Achievements":"4년제 학사"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"(주)아티프렌즈",
+        "specialization":"프론트엔드 개발자",
+        "MonthOfLeaving":"2021.08 ~ 현재",
+        "Achievements":"블록체인 지갑 앱 개발"
+      },
+      {
+        "CompanyName":"(주)동훈아이텍",
+        "specialization":"IT 보안솔루션 영업",
+        "MonthOfLeaving":"2021.01 ~ 2021.06",
+      },
+      {
+        "CompanyName":"(주)동양",
+        "specialization":"생산관리",
+        "MonthOfLeaving":"2020.09 ~ 2020.12",
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"JQuery"
+      },
+      {
+        "skillname":"React"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Guardee Blockchain Wallet",
+        "description":"블록체인 지갑 웹/앱 개발 및 유지보수",
+        "imgurl":"images/portfolio/guardee-app.png",
+        "url":"https://guardee.io"
+      },
+      {
+        "name":"Guardee Landing Page",
+        "description":"블록체인 지갑 앱 랜딩 페이지 개발 및 유지보수",
+        "imgurl":"images/portfolio/guardee-landing.png",
+        "url":"https://guardee.app"
+      },
+      {
+        "name":"Artifriends",
+        "description":"회사 소개 홈페이지 개발 및 유지보수",  
+        "imgurl":"images/portfolio/artihome.png",
+        "url":"https://artifriends.com"
+      },
+      {
+        "name":"Artifriends Admin homepage",
+        "description":"회사 어드민 페이지 개발 및 유지보수",
+        "imgurl":"images/portfolio/artiadmin.png",
+        "url":""
+      }
+    ],
+  }
+  
+  export default resumeData
