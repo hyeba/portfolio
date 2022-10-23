07984104b7e6a3442b3c579eef2ba6fbbe93e83c
HYEJIN3 <aslk1111zz@naver.com>
2022-10-23 23:43:08 +0900

    Updates

---
+++ precache-manifest.09bfc57f62bb191fbc10165583bccfde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c47cc54d0498d5518af3",
+    "url": "/portfolio/static/css/main.16751f12.chunk.css"
+  },
+  {
+    "revision": "c47cc54d0498d5518af3",
+    "url": "/portfolio/static/js/main.c47cc54d.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "64e047d40af68a501b447a84e6437f1d",
+    "url": "/portfolio/index.html"
+  }
+];
